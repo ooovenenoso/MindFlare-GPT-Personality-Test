0a23b8825672c45aaa2fb3a7a305f6f305bd1c29
ooovenenoso <120500656+ooovenenoso@users.noreply.github.com>
2023-10-17 14:49:46 -0400

    Update main.py
    Terms and Conditions, Graphical Interface & Other changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tkinter import Tk, Label, Button, Scale, messagebox, ttk, Entry, Frame
-from PIL import Image, ImageDraw, ImageFont
+from tkinter import Tk, Label, Button, Scale, messagebox, ttk, Entry, Frame, IntVar, Checkbutton, Scrollbar, Text, Toplevel, PhotoImage
+from PIL import Image, ImageDraw, ImageFont, ImageTk
 from fpdf import FPDF
 from datetime import datetime
 import openai
@@ -91,32 +91,127 @@ user_age = ""
 
 # Preguntas y respuestas y demás funciones se mantienen igual...
 
+def accept_terms_and_conditions():
+    def proceed():
+        if var.get():
+            terms_window.destroy()
+            collect_name_and_age()
+        else:
+            messagebox.showwarning("Términos y Condiciones", "Debes aceptar los términos y condiciones para continuar.")
+
+    terms_window = Tk()
+    terms_window.title("Términos y Condiciones")
+    terms_window.configure(bg="#f5f5f5")
+    terms_window.attributes('-fullscreen', True)
+
+    # Frame para contener el Text y Scrollbar
+    frame = Frame(terms_window, bg="#f5f5f5")
+    frame.pack(pady=20, padx=20, expand=True, fill="both")
+
+    scrollbar = Scrollbar(frame)
+    scrollbar.pack(side="right", fill="y")
+
+    terms_display = Text(frame, wrap="word", yscrollcommand=scrollbar.set, font=("Helvetica", 10), height=20, width=80)
+    terms_display.pack(side="left", expand=True, fill="both")
+
+    # Insertar texto con formato
+    terms_display.tag_configure("title", font=("Arial", 16, "bold"))
+    terms_display.tag_configure("bold", font=("Arial", 12, "bold"))
+    terms_display.tag_configure("normal", font=("Arial", 12))
+
+    terms_display.insert("1.0", "Términos y Condiciones del Test de Personalidad MindFlare GPT\n", "title")
+    terms_display.insert("end", "\n1. Objetivo del Test: ", "bold")
+    terms_display.insert("end", "El Test de Personalidad MindFlare GPT es una herramienta complementaria de evaluación diseñada para interpretar ciertas características de personalidad basadas en las respuestas proporcionadas. Es fundamental entender que los resultados son interpretativos y subjetivos, y no deben ser considerados como diagnósticos médicos, psicológicos o definitivos para la toma de decisiones.\n\n", "normal")
+    terms_display.insert("end", "2. Consentimiento Voluntario: ", "bold")
+    terms_display.insert("end", "Al iniciar el test, das tu consentimiento voluntario para participar. Tienes el derecho de no responder cualquier pregunta, detener el test en cualquier momento o no completarlo, sin enfrentar penalizaciones o discriminación por parte de la empresa evaluadora.\n\n", "normal")
+    terms_display.insert("end", "3. Privacidad y Tratamiento de Datos: ", "bold")
+    terms_display.insert("end", "Tu privacidad es esencial. Las respuestas que proporciones serán procesadas de forma anónima y la empresa garantizará la confidencialidad de tus datos. Sin tu consentimiento explícito, tus respuestas no serán conservadas más allá del tiempo necesario para el proceso de evaluación ni compartidas con terceros ajenos al proceso. Tienes el derecho de solicitar acceso a tus datos y, si es necesario, corregir o eliminar cualquier información incorrecta.\n\n", "normal")
+    terms_display.insert("end", "4. Limitación de Responsabilidad: ", "bold")
+    terms_display.insert("end", "Ni la empresa que proporciona este software ni la empresa que te está evaluando garantizan que los resultados del test sean un reflejo completo o preciso de tu personalidad o aptitud para un cargo. Los resultados no deben ser la única base para decisiones personales o profesionales. Ambas empresas se desligan de cualquier responsabilidad por decisiones tomadas basadas únicamente en los resultados de este test.\n\n", "normal")
+    terms_display.insert("end", "5. Derechos de Propiedad: ", "bold")
+    terms_display.insert("end", "Este test, incluido su contenido, diseño, gráficos y software, está protegido por derechos de propiedad intelectual. Está prohibido copiar, distribuir, modificar, revender o usar el contenido del test con fines comerciales sin el permiso adecuado.\n\n", "normal")
+    terms_display.insert("end", "6. Modificaciones: ", "bold")
+    terms_display.insert("end", "Nos reservamos el derecho de modificar estos términos y condiciones en cualquier momento sin previo aviso. Es responsabilidad del usuario revisar regularmente estos términos.\n\n", "normal")
+    terms_display.insert("end", "7. Terminación: ", "bold")
+    terms_display.insert("end", "Nos reservamos el derecho de suspender o terminar el acceso de un usuario a la herramienta por cualquier motivo, incluyendo la violación de estos términos.\n\n", "normal")
+    terms_display.insert("end", "8. Contacto: ", "bold")
+    terms_display.insert("end", "Si tienes preguntas o inquietudes acerca de este test o de tus resultados, debes dirigirte a la empresa que te está evaluando. Se esforzarán por responder a tus inquietudes en un plazo máximo de 15 días laborables.\n\n", "normal")
+    terms_display.insert("end", "9. Ley Aplicable y Jurisdicción: ", "bold")
+    terms_display.insert("end", "Estos términos se regirán según las leyes de Puerto Rico. Las partes acuerdan hacer todo lo posible para resolver cualquier disputa relacionada con estos términos, el test o sus resultados de manera amistosa antes de iniciar acciones legales. Si no se llega a un acuerdo, la disputa será resuelta en los tribunales de [Ciudad, País/Estado].\n\n", "normal")
+    terms_display.insert("end", "10. Licencia Creative Commons: ", "bold")
+    terms_display.insert("end", "Este test está licenciado bajo la Licencia Creative Commons Attribution-NonCommercial-NoDerivs 2.0 Generic (CC BY-NC-ND 2.0). Puedes compartir el test, pero no puedes hacer uso comercial de él ni realizar obras derivadas.\n\n", "normal")
+    terms_display.insert("end", "11. No Garantía: ", "bold")
+    terms_display.insert("end", "Este test se proporciona 'tal cual', sin garantías de ningún tipo, ya sean expresas o implícitas, incluyendo, pero no limitado a, garantías implícitas de comerciabilidad, idoneidad para un propósito particular o no infracción.\n\n", "normal")
+    terms_display.insert("end", "12. Indemnización: ", "bold")
+    terms_display.insert("end", "El usuario acepta indemnizar, defender y mantener indemne a la empresa, sus licenciantes, empleados, agentes y representantes de cualquier reclamación, pérdida, daño, coste, gasto y otra responsabilidad, incluyendo, sin limitación, honorarios legales, que surja del uso o mal uso del test, o la violación de estos términos.\n\n", "normal")
+    terms_display.insert("end", "13. Terceros: ", "bold")
+    terms_display.insert("end", "Estos términos no otorgan derechos a terceros beneficiarios. Sólo las partes en estos términos pueden hacer cumplir estos términos.\n\n", "normal")
+    terms_display.insert("end", "\n**Al utilizar esta herramienta, el usuario acepta estar vinculado por estos términos y condiciones y cualquier otro aviso o restricción relacionado con el uso de la herramienta.**\n", "bold")
+
+    terms_display.configure(state="disabled")
+    scrollbar.config(command=terms_display.yview)
+
+    var = IntVar()
+    check_button = Checkbutton(terms_window, text="Acepto los términos y condiciones", variable=var, bg="#f5f5f5", font=("Helvetica", 10))
+    check_button.pack(pady=10)
+
+    Button(terms_window, text="Continuar", command=proceed, bg="#4caf50", fg="white", relief="flat", font=("Helvetica", 10)).pack(pady=10)
+
+    terms_window.mainloop()
+
+
 # Ventana para recopilar nombre y edad
 def collect_name_and_age():
     def submit():
         global user_name, user_age
-        user_name = name_entry.get()
-        user_age = age_entry.get()
+        user_name = name_entry.get().strip()  # Eliminar espacios en blanco al principio y al final
+        user_age = age_entry.get().strip()
+
+        # Validar que los campos no estén vacíos
+        if not user_name or not user_age:
+            messagebox.showwarning("Información faltante", "Por favor, rellena todos los campos antes de continuar.")
+            return
+
+        # Validar que la edad sea un número
+        if not user_age.isdigit():
+            messagebox.showwarning("Edad inválida", "Por favor, introduce una edad válida (solo números).")
+            return
+
         user_info_window.destroy()
 
     user_info_window = Tk()
     user_info_window.title("Ingrese su Nombre y Edad")
     user_info_window.configure(bg="#f5f5f5")
+    
+    # Modo de pantalla completa
+    user_info_window.attributes('-fullscreen', True)
 
-    name_label = Label(user_info_window, text="Nombre Completo:", bg="#f5f5f5", font=("Helvetica", 10))
-    name_label.pack(pady=10)
-    name_entry = Entry(user_info_window, font=("Helvetica", 10))
-    name_entry.pack(pady=10)
+    # Marco central para agrupar los elementos y centrarlos
+    center_frame = Frame(user_info_window, bg="#f5f5f5")
+    center_frame.place(relx=0.5, rely=0.5, anchor="center")
 
-    age_label = Label(user_info_window, text="Edad:", bg="#f5f5f5", font=("Helvetica", 10))
-    age_label.pack(pady=10)
-    age_entry = Entry(user_info_window, font=("Helvetica", 10))
-    age_entry.pack(pady=10)
+    title_label = Label(center_frame, text="Información Básica: Identifícate", bg="#f5f5f5", font=("Arial", 20, "bold"))
+    title_label.pack(pady=30)
 
-    submit_button = Button(user_info_window, text="Enviar", command=submit, bg="#4caf50", fg="white", relief="flat", font=("Helvetica", 10))
-    submit_button.pack(pady=10)
+    name_frame = Frame(center_frame, bg="#f5f5f5")
+    name_frame.pack(pady=15, fill="x")
+    name_label = Label(name_frame, text="Nombre Completo:", bg="#f5f5f5", font=("Arial", 14))
+    name_label.grid(row=0, column=0, padx=(0, 20))
+    name_entry = Entry(name_frame, font=("Arial", 12), width=40)
+    name_entry.grid(row=0, column=1, padx=(20, 0))
+
+    age_frame = Frame(center_frame, bg="#f5f5f5")
+    age_frame.pack(pady=15, fill="x")
+    age_label = Label(age_frame, text="Edad:", bg="#f5f5f5", font=("Arial", 14), width=15)
+    age_label.grid(row=0, column=0, padx=(0, 20))
+    age_entry = Entry(age_frame, font=("Arial", 12), width=40)
+    age_entry.grid(row=0, column=1, padx=(20, 0))
+
+    submit_button = Button(center_frame, text="Enviar", command=submit, bg="#4caf50", fg="white", relief="flat", font=("Arial", 14, "bold"), width=20)
+    submit_button.pack(pady=40)
 
     user_info_window.mainloop()
+
 
 def next_question():
     save_answer()
@@ -131,6 +226,20 @@ def next_question():
     else:
         messagebox.showinfo("Test de Personalidad", "¡Has completado todas las preguntas!")
 
+def confirm_exit():
+    response = messagebox.askyesno("Confirmar salida", "¿Estás seguro de que quieres cerrar el programa?")
+    if response:
+        summary_window.destroy()  # Cerrar la ventana de resumen
+        root.destroy()  # Cerrar la ventana principal
+
+def save_as_pdf():
+    generate_graph()  # Primero genera el gráfico y lo guarda como imagen
+    summary_text = "Resumen de tus respuestas:\n\n"
+    for category, scores in answers.items():
+        avg_score = sum(scores) / len(scores)
+        summary_text += f"{category}: {avg_score:.2f}/7\n"
+    generate_pdf_with_summary(summary_text)  # Luego genera el PDF con el resumen y la imagen del gráfico
+
 def save_answer():
     global current_question, answers
     category_index = current_question // 12
@@ -142,22 +251,9 @@ def save_answer():
         answers[category] = []
     answers[category].append(answer)
 
-def previous_question():
-    global current_question
-    if current_question > 0:
-        current_question -= 1
-        category_index = current_question // 12
-        category = list(questions.keys())[category_index]
-        question = questions[category][current_question % 12]
-        question_label.config(text=question)
-        
-        # Restaurar la respuesta previamente guardada en la escala
-        previous_answer = answers[category][current_question % 12]
-        scale.set(previous_answer)
-    else:
-        messagebox.showinfo("Test de Personalidad", "Ya estás en la primera pregunta.")
 
 def generate_summary():
+    global summary_window
     summary_window = Tk()
     summary_window.title("Resumen del Test de Personalidad")
     
@@ -169,9 +265,9 @@ def generate_summary():
     
     Label(summary_window, text=summary_text, font=("Arial", 12)).pack(pady=20)
     
-    Button(summary_window, text="Guardar como PDF", command=lambda: generate_pdf_with_summary(summary_text)).pack(pady=10)
-    Button(summary_window, text="Ver en Gráfico", command=generate_graph).pack(pady=10)
-    Button(summary_window, text="Cerrar", command=summary_window.destroy).pack(pady=10)
+    Button(summary_window, text="Guardar como PDF", command=save_as_pdf).pack(pady=10)
+    # Button(summary_window, text="Ver en Gráfico", command=generate_graph).pack(pady=10)
+    Button(summary_window, text="Cerrar", command=confirm_exit).pack(pady=10)
 
 def get_openai_summary(answers):
     # Convertir las respuestas en texto
@@ -197,7 +293,7 @@ def get_openai_summary(answers):
 class MyPDF(FPDF):
     def header(self):
         # Ruta de la imagen de fondo
-        background_image = "background.png"
+        background_image = "media/background.png"
 
         # Obtener las dimensiones de la imagen (en mm)
         image_width, image_height = self.get_image_dimensions(background_image)
@@ -293,7 +389,7 @@ def generate_graph():
     fig.savefig(image_path, dpi=300, bbox_inches='tight')  # Aumentar la resolución y ajustar el tamaño
     
     # Mostrar el gráfico
-    plt.show()
+    # plt.show()
 
 def update_progressbar():
     total_questions = sum(len(v) for v in questions.values())
@@ -301,6 +397,11 @@ def update_progressbar():
     progress_bar['value'] = percentage_complete
 
 def reset_test():
+    # Ventana emergente de confirmación
+    response = messagebox.askyesno("Confirmar reinicio", "¿Estás seguro de que quieres reiniciar el test?")
+    if not response:
+        return  # Si el usuario selecciona 'No', no haremos nada y saldremos de la función
+
     global answers, current_question
     answers = {}  # Reiniciar las respuestas
     current_question = 0  # Reiniciar el contador de preguntas
@@ -308,19 +409,35 @@ def reset_test():
     scale.set(0)  # Reiniciar la escala
 
 # Recopilar nombre y edad antes de comenzar el test
-collect_name_and_age()
+accept_terms_and_conditions()
 
 # Ventana principal
 root = Tk()
 root.title("Test de Personalidad")
-root.geometry("700x500")
-root.configure(bg="#f5f5f5")  # Fondo moderno
+root.attributes('-fullscreen', True)
+root.configure(bg="#f5f5f5")
 
-question_label = Label(root, text="Presiona Siguiente para comenzar el test.", bg="#f5f5f5", font=("Helvetica", 14), fg="#333333")
+# Cargar y ajustar la única imagen que se usará
+original_image = Image.open("media/front.png")
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+resized_image = original_image.resize((screen_width, screen_height), Image.LANCZOS)
+front_image = ImageTk.PhotoImage(resized_image)
+
+# Marco centrado
+center_frame = Frame(root, bg="#f5f5f5")
+center_frame.place(relx=0.5, rely=0.5, anchor='center')
+
+question_label = Label(center_frame, text="Presiona Siguiente para comenzar el test.", bg="#f5f5f5", font=("Helvetica", 14), fg="#333333")
 question_label.pack(pady=20)
 
+# Fondo de la ventana (único layer)
+front_label = Label(root, image=front_image, bg="#f5f5f5")
+front_label.place(relx=0.5, rely=0.5, anchor='center')
+front_label.lower()  # Enviar la etiqueta al fondo
+
 # Escala y etiquetas
-scale_frame = Frame(root, bg="#f5f5f5")
+scale_frame = Frame(center_frame, bg="#f5f5f5")
 scale_frame.pack(pady=10)
 
 left_reference_label = Label(scale_frame, text="No me identifico", bg="#f5f5f5", font=("Helvetica", 10), fg="#555555")
@@ -333,14 +450,11 @@ right_reference_label = Label(scale_frame, text="Me identifico totalmente", bg="
 right_reference_label.pack(side="left", padx=5)
 
 # Botones
-button_frame = Frame(root, bg="#f5f5f5")
+button_frame = Frame(center_frame, bg="#f5f5f5")
 button_frame.pack(pady=10)
 
 next_button = Button(button_frame, text="Siguiente", command=lambda: [next_question(), update_progressbar()], bg="#4caf50", fg="white", relief="flat", font=("Helvetica", 10))
 next_button.pack(side="left", padx=5)
-
-previous_button = Button(button_frame, text="Anterior", command=previous_question, bg="#9e9e9e", fg="white", relief="flat", font=("Helvetica", 10))
-previous_button.pack(side="left", padx=5)
 
 finish_button = Button(button_frame, text="Finalizar", command=generate_summary, bg="#ff5722", fg="white", relief="flat", font=("Helvetica", 10))
 finish_button.pack(side="left", padx=5)
@@ -349,8 +463,7 @@ reset_button = Button(button_frame, text="Reiniciar", command=lambda: [reset_tes
 reset_button.pack(side="left", padx=5)
 
 # Barra de progreso
-progress_bar = ttk.Progressbar(root, orient="horizontal", length=600, mode="determinate", style="TProgressbar")
+progress_bar = ttk.Progressbar(center_frame, orient="horizontal", length=600, mode="determinate", style="TProgressbar")
 progress_bar.pack(pady=20)
 
 root.mainloop()
-
